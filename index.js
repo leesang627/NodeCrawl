fde7b562d10c101281107891a26dab1db5a3feb5
Sangwook Lee <leesang627@gmail.com>
2019-10-06 00:16:00 +0900

    2-6, userAgent 변경, for of 문과 waitFor로 인간다운 움직임으로 변경

--- index.js
+++ index.js
@@ -10,25 +10,23 @@ const crawler = async () => {
   try{
     const result = [];
     const browser = await puppeteer.launch({headless: process.env.NODE_ENV === 'production'});
-    await Promise.all(records.map(async (r,i) => {
-      try{
-        const page = await browser.newPage();
-        await page.goto(r[1]);
-        const text = await page.evaluate(() => {
-          const score = document.querySelector('.score.score_left .star_score');
-          if(score) {
-            return score.textContent;
-          }
-        });
-        if(text) {
-          console.log(r[0],'평점',text.trim());
-          result[i] = [r[0],r[1],text.trim()];
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36');
+    for (const [ i, r ] of records.entries()){
+      await page.goto(r[1]);
+      const text = await page.evaluate(() => {
+        const score = document.querySelector('.score.score_left .star_score');
+        if(score) {
+          return score.textContent;
         }
-        await page.close();
-      } catch (e) {
-        console.error(e);
+      });
+      if(text) {
+        console.log(r[0],'평점',text.trim());
+        result[i] = [r[0],r[1],text.trim()];
       }
-    }));
+      await page.waitFor(1000);
+    }
+    await page.close();
     const str = stringify(result);
     fs.writeFileSync('csv/result.csv', str);
     await browser.close();
