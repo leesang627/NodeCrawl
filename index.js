7e668a902a0c7dcb6eeb0f82c85b09c252a2a382
Sangwook Lee <leesang627@gmail.com>
2019-10-07 11:53:04 +0900

    4-2 completed

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const crawler = async () => {
     const page = await browser.newPage();
     await page.goto('https://unsplash.com');
     const result = await page.evaluate(() => {
-
-    })
+      let imgs = [];
+      const imgEls = document.querySelectorAll('._1Nk0C img._2zEKz')
+      if (imgEls.length) {
+        imgEls.forEach((v) => {
+          if(v.src) {
+            imgs.push(v.src);
+          }
+        });
+      }
+      return imgs;
+    });
+    console.log(result);
   } catch (e) {
     console.error(e);
   }
