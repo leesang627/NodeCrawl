14a7f796a13896ea192bb7d1cb765e0a33b61fa6
Sangwook Lee <leesang627@gmail.com>
2019-10-07 13:21:01 +0900

    4-4 completed

--- index.js
+++ index.js
@@ -8,19 +8,31 @@ const crawler = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://unsplash.com');
-    const result = await page.evaluate(() => {
-      let imgs = [];
-      const imgEls = document.querySelectorAll('._1Nk0C img._2zEKz')
-      if (imgEls.length) {
-        imgEls.forEach((v) => {
-          if(v.src) {
-            imgs.push(v.src);
-          }
-        });
-      }
-      return imgs;
-    });
+    let result = [];
+    while (result.length <= 100){
+      const srcs = await page.evaluate(() => {
+        window.scrollTo(0,0);
+        let imgs = [];
+        const imgEls = document.querySelectorAll('._1Nk0C');
+        if (imgEls.length) {
+          imgEls.forEach((v) => {
+            let src = v.querySelector('img._2zEKz').src;
+            if(src) {
+              imgs.push(src);
+            }
+            v.parentElement.removeChild(v);
+          });
+        }
+        window.scrollBy(0,300);
+        return imgs;
+      });
+      result = result.concat(srcs);
+      await page.waitForSelector('._1Nk0C');
+      console.log('로딩 완료');
+    }
     console.log(result);
+    await page.close();
+    await browser.close();
   } catch (e) {
     console.error(e);
   }
