cee4dd87a1dd7d6e317d14e5f9ab173825cc939d
Sangwook Lee <leesang627@gmail.com>
2019-10-07 11:32:32 +0900

    4-1 completed

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const puppeteer = require('puppeteer');
 
 const crawler = async () => {
   try{
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://unsplash.com');
+    const result = await page.evaluate(() => {
 
+    })
   } catch (e) {
     console.error(e);
   }
