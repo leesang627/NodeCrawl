5e7d26a04b4fd0a457cd2ee23f6aa2324e1cc09d
Sangwook Lee <leesang627@gmail.com>
2019-10-07 22:21:21 +0900

    facebook login, use dotenv, 5-2 completed

--- index.js
+++ index.js
@@ -1,50 +1,27 @@
 const fs = require('fs');
 const axios = require('axios');
+const dotenv = require('dotenv');
 const puppeteer = require('puppeteer');
 
-fs.readdir('imgs', (err) => {
-  if(err) {
-    console.error('imgs 폴더가 없어 새로 생성합니다.');
-    fs.mkdirSync('imgs');
-  }
-})
+dotenv.config();
 
 const crawler = async () => {
   try{
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: false, args: ['--window-size=1920,1080']});
     const page = await browser.newPage();
-    await page.goto('https://unsplash.com');
-    let result = [];
-    while (result.length <= 10){
-      const srcs = await page.evaluate(() => {
-        window.scrollTo(0,0);
-        let imgs = [];
-        const imgEls = document.querySelectorAll('._1Nk0C');
-        if (imgEls.length) {
-          imgEls.forEach((v) => {
-            let src = v.querySelector('img._2zEKz').src;
-            if(src) {
-              imgs.push(src);
-            }
-            v.parentElement.removeChild(v);
-          });
-        }
-        window.scrollBy(0,300);
-        return imgs;
-      });
-      result = result.concat(srcs);
-      await page.waitForSelector('._1Nk0C');
-      console.log('로딩 완료');
-    }
-    console.log(result);
-    result.forEach( async (src) => {
-      const imgResult = await axios.get(src.replace(/\?.*$/, ''), {
-        responseType: 'arraybuffer',
-      });
-      fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imgResult.data);
-    });
-    await page.close();
-    await browser.close();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    })
+    await page.goto('https://www.facebook.com');
+    const id = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    await page.evaluate((id, password) => {
+      document.querySelector('#email').value = id;
+      document.querySelector('#pass').value = password;
+      document.querySelector('#loginbutton').click();
+    }, id, password)
+
   } catch (e) {
     console.error(e);
   }
