e8126919759b4abc3e2c3839f8c74bc2788004fa
Sangwook Lee <leesang627@gmail.com>
2019-10-08 12:51:23 +0900

    프록시 사이트 크롤링을 통해 가장 빠른 프록시를 찾아서 접속하기, 6-2 completed

--- index.js
+++ index.js
@@ -7,90 +7,36 @@ dotenv.config();
 
 const crawler = async () => {
   try{
-    const browser = await puppeteer.launch({headless: false, args: ['--window-size=1920,1080','--disable-notifications']});
-    const page = await browser.newPage();
+    let browser = await puppeteer.launch({headless: false, args: ['--window-size=1920,1080','--disable-notifications']});
+    let page = await browser.newPage();
     await page.setViewport({
       width: 1080,
       height: 1080,
-    })
-    await page.goto('https://www.facebook.com');
-    await page.evaluate(() => {
-      (() => {
-        const box = document.createElement('div');
-        box.classList.add('mouse-helper');
-        const styleElement = document.createElement('style');
-        styleElement.innerHTML = `
-          .mouse-helper {
-            pointer-events: none;
-            position: absolute;
-            z-index: 100000;
-            top: 0;
-            left: 0;
-            width: 20px;
-            height: 20px;
-            background: rgba(0,0,0,.4);
-            border: 1px solid white;
-            border-radius: 10px;
-            margin-left: -10px;
-            margin-top: -10px;
-            transition: background .2s, border-radius .2s, border-color .2s;
-          }
-          .mouse-helper.button-1 {
-            transition: none;
-            background: rgba(0,0,0,0.9);
-          }
-          .mouse-helper.button-2 {
-            transition: none;
-            border-color: rgba(0,0,255,0.9);
-          }
-          .mouse-helper.button-3 {
-            transition: none;
-            border-radius: 4px;
-          }
-          .mouse-helper.button-4 {
-            transition: none;
-            border-color: rgba(255,0,0,0.9);
-          }
-          .mouse-helper.button-5 {
-            transition: none;
-            border-color: rgba(0,255,0,0.9);
-          }
-          `;
-        document.head.appendChild(styleElement);
-        document.body.appendChild(box);
-        document.addEventListener('mousemove', event => {
-          box.style.left = event.pageX + 'px';
-          box.style.top = event.pageY + 'px';
-          updateButtons(event.buttons);
-        }, true);
-        document.addEventListener('mousedown', event => {
-          updateButtons(event.buttons);
-          box.classList.add('button-' + event.which);
-        }, true);
-        document.addEventListener('mouseup', event => {
-          updateButtons(event.buttons);
-          box.classList.remove('button-' + event.which);
-        }, true);
-        function updateButtons(buttons) {
-          for (let i = 0; i < 5; i++)
-            box.classList.toggle('button-' + i, !!(buttons & (1 << i)));
-        }
-      })();
     });
-    await page.type('#email', process.env.EMAIL);
-    await page.type('#pass', process.env.PASSWORD);
-    await page.mouse.move(1000,40);
-    await page.waitFor(1000);
-    await page.mouse.click(1000,40);
-    // await page.waitFor(2000);
-    // await page.click('#loginbutton');
-    await page.waitFor(3000);
-    await page.click('#userNavigationLabel');
-    await page.waitForSelector('li.navSubmenu:last-child');
-    await page.click('li.navSubmenu:last-child');
-    await page.waitFor(3000);
+    await page.goto('http://spys.one/free-proxy-list/KR/');
+    const proxies = await page.evaluate(() => {
+      const ips = Array.from(document.querySelectorAll('tr > td:first-of-type > .spy14')).map(v => {
+        return v.textContent.replace(/document\.write\(.+\)/,'');
+      });
+      const latencies = Array.from(document.querySelectorAll('tr > td:nth-of-type(6) .spy1')).map(v => v.textContent);
+      const types = Array.from(document.querySelectorAll('tr > td:nth-of-type(2)')).slice(4,34).map(v => v.textContent);
+      return ips.map((v,i) => {
+        return {
+          ip: v,
+          type: types[i],
+          latency: latencies[i],
+        };
+      });
+    });
+    const proxyList = proxies.filter(v => v.type.startsWith('HTTPS')).sort((p,c) => p.latency-c.latency);
+    console.log(proxyList);
     await page.close();
     await browser.close();
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ['--window-size=1920,1080','--diable-notifications',`--proxy-server=${proxyList[0].ip}`],
+    });
+    page = await browser.newPage();
   } catch (e) {
     console.error(e);
   }
